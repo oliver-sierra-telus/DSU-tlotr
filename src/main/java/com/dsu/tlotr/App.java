89dceb5e6daf0df00a35cbc05b320d69cffb538e
oliver-sierra-telus <oliver.sierra@telus.com>
2023-02-16 16:54:08 -0600

    first commit

---
+++ src/main/java/com/dsu/tlotr/App.java
@@ -0,0 +1,13 @@
+package com.dsu.tlotr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
